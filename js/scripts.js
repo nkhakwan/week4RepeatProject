7be4f5e1eba4b16f38e76a3de49743e4e00e9cd1
nkhakwan <post_khan@yahoo.com>
2020-04-10 11:44:27 -0700

    this'll push bare minimum complete project

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
 // Business Logic -----------------------
 function PizzaOrders() {
-  this.toppingOne = [],
-  this.toppingTwo = [],
-  this.size =[],
-  this.currentId = 0
+  this.toppingOnePrice = [],
+  this.toppingTwoPrice = [],
+  this.sizePrice =[],
+  this.orderPrice = [],
+  this.id = 0
+}
+
+
+PizzaOrders.prototype.addOrder = function(toppingOne, toppingTwo, size, orderPrice) {
+  this.id += 1;
+  this.toppingOnePrice[this.id] = toppingOne;
+  this.toppingTwoPrice[this.id] = toppingTwo;
+  this.sizePrice[this.id] = size;
+  this.orderPrice[this.id] = orderPrice;
+  
+var calculateOrder = function(size, toppingOne, toppingTwo){
+  var price = size + toppingOne +toppingTwo;
+  return price;
 }
 
 // user interface logic -------------------------------
-
+var pizzaOrder = new PizzaOrders();
 $(document).ready(function() {
   $("form").submit(function(event){
+    alert("inthesedconloop");
     event.preventDefault();
-    //var pizzaSize = parseInt($("#size").val());
     var pizzaSize = parseInt($("#size").val());
     var meatType  = parseInt($("#meat").val());
     var vegType   = parseInt($("#veg").val());
     alert(pizzaSize + ","+ meatType +"," + vegType);
-    //var newTicket = new Ticket (agePrice, moviePrice, showTimePrice);
-    
-    //newTicket.addPrices();
+    var orderPrice = calculateOrder(pizzaSize,meatType,vegType);
+    pizzaOrder.addOrder(pizzaSize,meatType,vegType,orderPrice);
+    alert(orderPrice);
   });
 });
