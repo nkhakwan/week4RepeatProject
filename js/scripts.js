af78c6ca0636efccaebae85dda9374f9e79bdacc
nkhakwan <post_khan@yahoo.com>
2020-04-10 13:32:29 -0700

    this'll push enhancements not working yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Business Logic -----------------------
 function PizzaOrders() {
-  this.toppingOnePrice = [],
-  this.toppingTwoPrice = [],
-  this.sizePrice =[],
+  this.toppingOne = [],
+  this.toppingTwo = [],
+  this.size =[],
   this.orderPrice = [],
   this.id = 0
 }
@@ -10,15 +10,58 @@ function PizzaOrders() {
 
 PizzaOrders.prototype.addOrder = function(toppingOne, toppingTwo, size, orderPrice) {
   this.id += 1;
+  if(toppingOne === 5){
+    this.toppingOne[this.id] = "Mutton";
+  } else if (toppingOne === 2)  {
+      this.toppingOne[this.id] = "Beef";
+    } else{
+      this.toppingOne[this.id] = "Chicken";
+    }
+  if(toppingTwo === 5){
+    this.toppingTwo[this.id] = "Olives And Artichoke";
+  } else {
+      this.toppingTwo[this.id] = "Mushrooms and Garden Salad";
+    }
+  if(size === 18){
+    this.size[this.id] = "Family size";
+  } else if (size === 15)  {
+      this.size[this.id] = "Large";
+    } else if (size === 12){
+      this.size[this.id] = "medium";
+      } else{
+        this.size[this.id] = "small";
+        }
+  this.orderPrice[this.id] = orderPrice;
+}
+
+
+
+/*PizzaOrders.prototype.addOrder = function(toppingOne, toppingTwo, size, orderPrice) {
+  this.id += 1;
   this.toppingOnePrice[this.id] = toppingOne;
   this.toppingTwoPrice[this.id] = toppingTwo;
   this.sizePrice[this.id] = size;
   this.orderPrice[this.id] = orderPrice;
-}
+}*/
 var calculateOrder = function(size, toppingOne, toppingTwo){
   var price = size + toppingOne +toppingTwo;
   return price;
 }
+
+
+/*function displayContactDetails(addressBookToDisplay) {
+  var contactsList = $("ul#contacts");
+  var htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "  " + contact.email + " " + "</li>" ;
+  contactsList.html(htmlForContactInfo);
+});
+contactsList.html(htmlForContactInfo);
+};*/
+
+
+
+
 
 // user interface logic -------------------------------
 var pizzaOrder = new PizzaOrders();
